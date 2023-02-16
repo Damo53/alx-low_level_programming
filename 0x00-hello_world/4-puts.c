15d988618711125fbe44478a09d9e64c67eb4873
damo53 <daamoshood.od@gmail.com>
2023-02-16 19:50:35 +0100

    first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+ *
+ * main - print out a code using puts
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+		
+}
+
