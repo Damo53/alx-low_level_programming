3b1ae1bccfd80eaa646f63f446b7dd4d6e0322ee
damo53 <daamoshood.od@gmail.com>
2023-02-22 00:48:52 +0100

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - entry point
+ *@n: int to be checked
+ *
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
+
