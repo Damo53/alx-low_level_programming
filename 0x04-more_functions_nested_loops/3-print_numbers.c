0f18517142e9f0067b41c5e8c1a94699c2e2a22d
damo53 <daamoshood.od@gmail.com>
2023-02-25 16:02:21 +0100

    task-3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers from 0- 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int c;
+
+	c = '0';
+	while (c <= '9')
+	{
+		_putchar(c + '0');
+		c++;
+	}
+	_putchar('\n');
+}
