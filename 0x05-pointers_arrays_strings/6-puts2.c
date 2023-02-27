8a889ad548afe2ac0f3274b3f6acf3d14ed01bee
damo53 <daamoshood.od@gmail.com>
2023-02-28 00:13:34 +0100

    task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: output
+ * Return: 0
+ */
+void puts2(char *str)
+{
+	int a;
+
+	a = 0;
+	while (str[a] >= 0)
+	{
+		a += 2;
+	_putchar(str[a]);
+	}
+	_putchar('\n');
+}
+
