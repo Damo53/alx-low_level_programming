50c1a6b5511b8365b5a9e8204cdf1dc0483a8e47
damo53 <daamoshood.od@gmail.com>
2023-03-05 13:34:45 +0100

    task4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the content of an array
+ * @a: first string
+ * @n: number of elements to swap
+ * Return: a
+ */
+void reverse_array(int *a, int n)
+{
+	int array;/*to store the arrays before swaping it*/
+	int i; /*position of element in the array*/
+
+
+	i = 0;
+	while (i < n / 2)
+	{
+	a[i] = array;
+	a[i] = a[n - 1 - i];
+	a[n - 1 - i] = array;
+	}
+}
